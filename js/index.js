d8be4cb05d29d143b8333d0832a0c4cba883b930
jsonpaker <42995724+jsonpaker@users.noreply.github.com>
2018-09-05 21:24:39 +0900

    123

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
  $(window).load(function(){
         $('.header').display="";
-        $('.header').fadeIn('slow',function(){
-            
-            
-        });
-        
+        $('.header').fadeIn('slow',function(){ });
+      $('#women').display="";
+        $('#women').fadeIn('slow',function(){ });
+      $('#men').display="";
+        $('#men').fadeIn('slow',function(){ });
+    
+     
     });
